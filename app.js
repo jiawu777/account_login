5951a350831f95e9255de3f69eb06f8fb8d525b8
wujia777 <cwu005104@gmail.com>
2022-08-17 20:08:51 +0800

    feat: project init

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const PORT = 3000
 const app = express()
-app.listen(PORT, () => console.log(`Please login in http://localhost:${PORT}`))
+
+app.get('/', (req, res) => {
+    res.send('hello Jia 123!')
+})
+
+app.listen(PORT, () => console.log(`Express is running on http://localhost:${PORT}`))
