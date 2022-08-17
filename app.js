fbba0401c286b9ed02e7174fd47a4a0c478533e3
wujia777 <cwu005104@gmail.com>
2022-08-17 20:43:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongoose error!')
+})
+
+db.once('open', () => {
+    console.log('mongoose connected!')
+})
+
 app.get('/', (req, res) => {
     res.send('hello Jia 123!')
 })
